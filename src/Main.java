743a284b1c1f5a38632f8e61a0a2c94f7dbb0315
Davi da Silva <davi.dasilva@colostate.edu>
2026-02-26 16:33:54 -0700

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        User newUser = new User(123);
-        System.out.println(newUser.getBalance());
-        System.out.println(newUser.checkPassword(123));
-    }
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("Jack", 100));
+        users.add(new User("Bob", 123));
 
+        User jack = users.get(0);
+        User bob = users.get(1);
+
+        System.out.println(jack.getUsername());
+        System.out.println(jack.getBalance());
+
+        System.out.println(bob.getUsername());
+        System.out.println(bob.getBalance());
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,25 +1,55 @@
-public class User {
-    private double  balance;
-    private int password;
+import java.util.ArrayList;
 
-    public User(int password) {
-        this.balance = 1000;
+public class User {
+    private String username;
+    private int password;
+    private double  balance;
+    private ArrayList<String> transactions;
+
+    public User(String username, int password) {
+        this.username = username;
         this.password = password;
+        this.balance = 0;
+        this.transactions = new ArrayList<>();
     }
 
-    private int getPassword() {
-        return password;
+    public String getUsername() {
+        return username;
     }
 
     public double getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
+    public ArrayList<String> getTransactions() {
+        return transactions;
     }
 
-    public boolean checkPassword(int inputPassword) {
-        return password == inputPassword;
+    public boolean checkPassword(int input) {
+        return password == input;
+    }
+
+    public boolean deposit(int amount) {
+        if (amount <= 0) {
+            return false;
+        } else {
+            balance += amount;
+            return true;
+            // Add transaction Array
+        }
+    }
+
+    public boolean withdraw(int amount) {
+        if (amount <= 0) {
+            return false;
+        } else {
+            balance -= amount;
+            return true;
+            // Add transaction Array
+        }
+    }
+
+    private void addTransaction(String type, int amount) {
+        // Use date and time, maybe there are libraries to do it for me?
     }
 }
